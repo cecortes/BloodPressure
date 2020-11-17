46fecd43d3ff0d8bd0f13e58add0029c00293f70
César López Cortés <cesarlopezcortes@hotmail.com>
2020-11-16 18:27:39 -0600

    Create get_blood.php

---
+++ Php/get_blood.php
@@ -0,0 +1,31 @@
+<?PHP
+$hostname="localhost";
+$database="bp";
+$username="root";
+$password="sylka1234";
+
+$json=array();
+
+$conexion=mysqli_connect($hostname,$username,$password,$database);
+$consulta="SELECT * FROM blood";
+$resultado=mysqli_query($conexion,$consulta);
+
+if($consulta){
+
+    if($reg=mysqli_fetch_array($resultado)){
+        $json['bp'][]=$reg;
+    }
+
+    mysqli_close($conexion);
+    echo json_encode($json);
+}else{
+    $results["id"]='';
+    $results["sisto"]='';
+    $results["diasto"]='';
+    $results["edo"]='';
+    $results["created"]='';
+    $json['bp'][]=$results;
+    echo json_encode($json);
+}
+
+?>
